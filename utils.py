8c4b39193636a01b7274ecd4f75584e00b06aad7
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2021-12-25 01:20:02 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,27 +1,34 @@
 from datetime import datetime
 import random 
+
+import os
+import subprocess as sp
+
 opening_text = [
     "Cool, I'm on it sir.",
     "Okay sir, I'm working on it.",
     "Just a second sir.",
-]
-import os
-import subprocess as sp
+     ]
 
 paths = {
     'notepad': "C:\\Program Files\\Notepad++\\notepad++.exe",
     'calculator': "C:\\Windows\\System32\\calc.exe"
-}
+     }
+
+inform = ''.join((random.choice(opening_text)) for x in range(1))
+
 def open_camera():
-    sp.run('start microsoft.windows.camera:', shell=True)
+    sp.run('start microsoft.windows.camera:', hell=True)
+
 def open_notepad():
     os.startfile(paths['notepad'])
+    
 def open_cmd():
     os.system('start cmd')
+
 def open_calculator():
     sp.Popen(paths['calculator'])
-inform = ''.join((random.choice(opening_text)) for x in range(1))
-Username=input("May I have your name please? \n")
+
 def greet_user():
     """Greets the user according to the time"""
     
@@ -32,10 +39,9 @@ def greet_user():
         print("Good Afternoon Mr.",(Username),".")
     elif (hour >= 16) and (hour <24 ):
         print("Good Evening Mr.",(Username),".")
-def take_user_input():
-    """Takes user input, recognizes it using Speech Recognition module and converts it into text"""
-    
 
+def take_user_input:
+    """Takes user input"""
     query = input("I am Jarvis. How may I assist you? \n")
     if "exit" in query or "stop" in query:
         hour = datetime.now().hour
@@ -50,12 +56,15 @@ def take_user_input():
         elif "camera" in open_task:
             open_camera()
         elif "calculator" in open_task:
-                open_calculator()
+            open_calculator()
         elif "command prompt" in open_task:
             open_cmd()
         elif "notepad" in open_task:
             open_notepad()
     else:
         print(inform)  
-   
     return query
+
+Username=input("May I have your name please? \n")
+greet_user()
+take_user_input
