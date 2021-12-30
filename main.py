4d202f8bfa10aac5213f3ccb595473e32861e8d1
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2021-12-31 00:57:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,46 @@
 import utils
+from datetime import datetime
+from utils import open_calculator, open_camera, open_cmd, open_notepad
+from info import opening_text, path, inform
+from pprint import pprint
+
+def greet_user():
+    """Greets the user according to the time"""
+    
+    hour = datetime.now().hour
+    if (hour >= 4) and (hour < 12):
+        print("Good Morning Mr.",(Username),".")
+    elif (hour >= 12) and (hour < 16):
+        print("Good Afternoon Mr.",(Username),".")
+    elif (hour >= 16) and (hour <24 ):
+        print("Good Evening Mr.",(Username),".")
+
+def take_user_input:
+    """Takes user input"""
+    query = input("I am Jarvis. How may I assist you? \n")
+    if "exit" in query or "stop" in query:
+        hour = datetime.now().hour
+        if hour >= 21 and hour < 6:
+            print("Good night sir, take care!")
+        else:
+            print('Have a good day sir!')
+    elif "Start" in query:
+        open_task = input("The options are camera,calculator, command prompt or notepad\n")
+        if "exit" in query or "stop" in query:
+            take_user_input()
+        elif "camera" in open_task:
+            open_camera()
+        elif "calculator" in open_task:
+            open_calculator()
+        elif "command prompt" in open_task:
+            open_cmd()
+        elif "notepad" in open_task:
+            open_notepad()
+    else:
+        print(inform)  
+    return query
+
+Username=input("May I have your name please? \n")
+greet_user()
+take_user_input
 
