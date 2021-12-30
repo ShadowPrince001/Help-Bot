6be535efaca8fe939026637de35182edf1c3eb3d
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2021-12-31 01:19:25 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,8 +4,13 @@ import random
 import os
 import subprocess as sp
 
+paths = {
+    'notepad': "C:\\Program Files\\Notepad++\\notepad++.exe",
+    'calculator': "C:\\Windows\\System32\\calc.exe"
+     }
+
 def open_camera():
-    sp.run('start microsoft.windows.camera:', hell=True)
+    sp.run('start microsoft.windows.camera:', shell=True)
 
 def open_notepad():
     os.startfile(paths['notepad'])
