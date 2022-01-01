edc051d209a61a87ad72ec876c2fe9bd74c28c14
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2022-01-01 14:17:03 +0530

    Update alt.py

--- alt.py
+++ alt.py
@@ -2,7 +2,7 @@ import utils
 from datetime import datetime
 from utils import open_calculator, open_camera, open_cmd, open_notepad
 from info import opening_text, paths
-import random 
+import random
 
 import os
 import subprocess as sp
@@ -10,35 +10,43 @@ import subprocess as sp
 
 def greet_user():
     """Greets the user according to the time"""
-    
+    username = input("May I have your name please? \n")
     hour = datetime.now().hour
     if (hour >= 0) and (hour < 12):
-        print("Good Morning Mr.",(username),".")
+        print("Good Morning Mr.", (username), ".I am AVA. ")
     elif (hour >= 12) and (hour < 16):
-        print("Good Afternoon Mr.",(username),".")
-    elif (hour >= 16) and (hour <24 ):
-        print("Good Evening Mr.",(username),".")
+        print("Good Afternoon Mr.", (username), ".I am AVA. ")
+    elif (hour >= 16) and (hour < 24):
+        print("Good Evening Mr.", (username), ".I am AVA. ")
+
 
 def take_user_input():
-    query = input("I am AVA. How may I assist you? \n")
-    if "exit" in query or "stop" in query:
+    query = input("How may I assist you? \n")
+    if not "exit" in query or "stop" in query:
+        print(random.choice(opening_text))
+    else:
         hour = datetime.now().hour
         if hour >= 21 and hour < 6:
             print("Good night sir, take care!")
         else:
-            print('Have a good day sir!')
-    elif "open camera" in query:
-        open_camera()
-    elif "open calculator" in query:
-        open_calculator()
-    elif "open command prompt" or "open cmd" in query:
-        open_cmd()
-    elif "open notepad" in query:
-        open_notepad()
-    else:
-        print(random.choice(opening_text))  
+            print("Have a good day sir!")
+        exit()
     return query
 
-username=input("May I have your name please? \n")
-greet_user()
-take_user_input()
+
+if __name__ == "__main__":
+    greet_user()
+    while True:
+        query = take_user_input()
+        if "open camera" in query:
+            open_camera()
+        elif "open calculator" in query:
+            open_calculator()
+        elif "open command prompt" in query:
+            open_cmd()
+        elif "open notepad" in query:
+            open_notepad()
+        else:
+            print("Sorry, I could not understand you Sir.")
+            query = "None"
+
